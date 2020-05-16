2aa97daf95c00dc8345aded6f5a1aab31de6e1af
Amrita Panesar <09panesara@gmail.com>
2020-05-16 12:44:34 +0100

    fixed gpu error with confusion matrix

--- src/lightning_model.py
+++ src/lightning_model.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.append('./')
 
 from pytorch_lightning.core.lightning import LightningModule
@@ -24,6 +25,7 @@ early_stop_callback = EarlyStopping(
     verbose=False,
     mode='min'
 )
+
 
 class L_STGCN(LightningModule):
     """
@@ -51,26 +53,36 @@ class L_STGCN(LightningModule):
             # initialise Masks for each stgcn layer as trainable parameter in network
             # TODO: don't hardcode 10, use numlayers
             # TODO: this is a bit spaghetti, add a comment or create a function for this
-            jitters = [ 2 * (torch.randn_like(A) - 0.5) * hparams.max_mask_jitter for i in range(10)]
-            self.Masks = nn.ParameterList([nn.Parameter( jitters[i] + torch.ones(A.shape)) for i in range(10)])
+            jitters = [2 * (torch.randn_like(A) - 0.5) * hparams.max_mask_jitter for i in range(10)]
+            self.Masks = nn.ParameterList([nn.Parameter(jitters[i] + torch.ones(A.shape)) for i in range(10)])
         else:
-            self.Masks = [torch.ones(A.shape) for i in range(10)] # not trainable
+            self.Masks = [torch.ones(A.shape) for i in range(10)]  # not trainable
 
         residual = self.hparams.residual
         dropout_rate = self.hparams.dropout_rate
 
         # Build the network
         self.conv = nn.Sequential(
-            SpatialTemporalConv(hparams.C_in, 64, A*self.Masks[0], hparams.gamma, 1, temporal_padding, dropout_rate=dropout_rate, residual=residual),
-            SpatialTemporalConv( 64,  64, A*self.Masks[1], hparams.gamma, 1, temporal_padding, dropout_rate=dropout_rate, residual=residual),
-            SpatialTemporalConv( 64,  64, A*self.Masks[2], hparams.gamma, 1, temporal_padding, dropout_rate=dropout_rate, residual=residual),
-            SpatialTemporalConv( 64,  64, A*self.Masks[3], hparams.gamma, 1, temporal_padding, dropout_rate=dropout_rate, residual=residual),
-            SpatialTemporalConv( 64, 128, A*self.Masks[4], hparams.gamma, 2, temporal_padding, dropout_rate=dropout_rate, residual=residual),
-            SpatialTemporalConv(128, 128, A*self.Masks[5], hparams.gamma, 1, temporal_padding, dropout_rate=dropout_rate, residual=residual),
-            SpatialTemporalConv(128, 128, A*self.Masks[6], hparams.gamma, 1, temporal_padding, dropout_rate=dropout_rate, residual=residual),
-            SpatialTemporalConv(128, 256, A*self.Masks[7], hparams.gamma, 2, temporal_padding, dropout_rate=dropout_rate, residual=residual),
-            SpatialTemporalConv(256, 256, A*self.Masks[8], hparams.gamma, 1, temporal_padding, dropout_rate=dropout_rate, residual=residual),
-            SpatialTemporalConv(256, 256, A*self.Masks[9], hparams.gamma, 1, temporal_padding, dropout_rate=dropout_rate, residual=residual)
+            SpatialTemporalConv(hparams.C_in, 64, A * self.Masks[0], hparams.gamma, 1, temporal_padding,
+                                dropout_rate=dropout_rate, residual=residual),
+            SpatialTemporalConv(64, 64, A * self.Masks[1], hparams.gamma, 1, temporal_padding,
+                                dropout_rate=dropout_rate, residual=residual),
+            SpatialTemporalConv(64, 64, A * self.Masks[2], hparams.gamma, 1, temporal_padding,
+                                dropout_rate=dropout_rate, residual=residual),
+            SpatialTemporalConv(64, 64, A * self.Masks[3], hparams.gamma, 1, temporal_padding,
+                                dropout_rate=dropout_rate, residual=residual),
+            SpatialTemporalConv(64, 128, A * self.Masks[4], hparams.gamma, 2, temporal_padding,
+                                dropout_rate=dropout_rate, residual=residual),
+            SpatialTemporalConv(128, 128, A * self.Masks[5], hparams.gamma, 1, temporal_padding,
+                                dropout_rate=dropout_rate, residual=residual),
+            SpatialTemporalConv(128, 128, A * self.Masks[6], hparams.gamma, 1, temporal_padding,
+                                dropout_rate=dropout_rate, residual=residual),
+            SpatialTemporalConv(128, 256, A * self.Masks[7], hparams.gamma, 2, temporal_padding,
+                                dropout_rate=dropout_rate, residual=residual),
+            SpatialTemporalConv(256, 256, A * self.Masks[8], hparams.gamma, 1, temporal_padding,
+                                dropout_rate=dropout_rate, residual=residual),
+            SpatialTemporalConv(256, 256, A * self.Masks[9], hparams.gamma, 1, temporal_padding,
+                                dropout_rate=dropout_rate, residual=residual)
         ).float()
 
         self.fc_layer = nn.Linear(256, hparams.nr_classes).float()
@@ -86,13 +98,13 @@ class L_STGCN(LightningModule):
         Returns:
             the results of classification
         """
-        x = x.permute(0, 3, 1, 2) # (N, C_in, T, V)
-        x = self.conv(x) # (N, C_out, T, V)
+        x = x.permute(0, 3, 1, 2)  # (N, C_in, T, V)
+        x = self.conv(x)  # (N, C_out, T, V)
         N, _, T, _ = x.shape
         # Global pooling. Can't be added to Sequential as the kernel size depends on x.
-        x = F.avg_pool2d(x, (x.shape[2], self.V)) # (N, C_out, 1, 1)
-        x = x.view(N, x.shape[1]) # (N, C_out)
-        x = self.fc_layer(x) # (N, nr_classes)
+        x = F.avg_pool2d(x, (x.shape[2], self.V))  # (N, C_out, 1, 1)
+        x = x.view(N, x.shape[1])  # (N, C_out)
+        x = self.fc_layer(x)  # (N, nr_classes)
 
         # x = self.softmax(x) # (N, 1)
         # don't need softmax in training if we use cross entropy as cross entropy does softmax on pred implicitly
@@ -110,7 +122,12 @@ class L_STGCN(LightningModule):
 
     def compute_conf_mat(self, dataloader):
         confusion_matrix = torch.zeros(self.nr_classes, self.nr_classes)
+        device = self.hparams.device
+        gpus = self.hparams.gpus
+
         for i, (x, y) in enumerate(dataloader):
+            if gpus > 0:
+                x = x.to(device)
             pred = self.forward(x)
             pred = torch.argmax(pred, axis=1)
 
@@ -118,43 +135,42 @@ class L_STGCN(LightningModule):
                 confusion_matrix[i, j] += 1
         return confusion_matrix
 
-
     def prepare_data(self):
         if hparams.augment_data:
             transforms = augment_data
         else:
             transforms = None
 
-        #TODO (rajmund): we shouldn't duplicate the datasets
+        # TODO (rajmund): we shouldn't duplicate the datasets
         # proposal: check if network is in training mode, if it isn't, don't augment. should be an easy fix!
         splitter = SplitDataset(self.hparams.metadata_file)
 
         ''' decide how to split the data for train, val, test set '''
-        if self.hparams.data_split == 0: # cross-subject training, default
+        if self.hparams.data_split == 0:  # cross-subject training, default
             train_ind, val_ind, test_ind = splitter.split_by_subject()
-        elif self.hparams.data_split == 1: # cross-scenario training
-            train_ind, val_ind, test_ind = splitter.split_by_scenario(self.hparams.train_scenarios, self.hparams.val_scenarios)
-        else: # stratified split
+        elif self.hparams.data_split == 1:  # cross-scenario training
+            train_ind, val_ind, test_ind = splitter.split_by_scenario(self.hparams.train_scenarios,
+                                                                      self.hparams.val_scenarios)
+        else:  # stratified split
             train_ind, val_ind, test_ind = splitter.split()
 
+        self.train_dataset = KTHDataset(metadata_csv_path=self.hparams.metadata_file,
+                                        numpy_data_folder=self.hparams.dataset_dir,
+                                        filter=train_ind,
+                                        transforms=transforms,
+                                        use_confidence_scores=False)  # TODO @rajmund:  confidence scores?
 
-        self.train_dataset = KTHDataset(metadata_csv_path = self.hparams.metadata_file,
-                                        numpy_data_folder = self.hparams.dataset_dir,
-                                        filter = train_ind,
-                                        transforms = transforms,
-                                        use_confidence_scores = False) #TODO @rajmund:  confidence scores?
+        self.val_dataset = KTHDataset(metadata_csv_path=self.hparams.metadata_file,
+                                      numpy_data_folder=self.hparams.dataset_dir,
+                                      filter=val_ind,
+                                      transforms=None,  # No augmentation during validation!
+                                      use_confidence_scores=False)
 
-        self.val_dataset   = KTHDataset(metadata_csv_path = self.hparams.metadata_file,
-                                        numpy_data_folder = self.hparams.dataset_dir,
-                                        filter = val_ind,
-                                        transforms = None, # No augmentation during validation!
-                                        use_confidence_scores = False)
-
-        self.test_dataset  = KTHDataset(metadata_csv_path = self.hparams.metadata_file,
-                                        numpy_data_folder = self.hparams.dataset_dir,
-                                        filter = test_ind,
-                                        transforms = None, # No augmentation during testing!
-                                        use_confidence_scores = False)
+        self.test_dataset = KTHDataset(metadata_csv_path=self.hparams.metadata_file,
+                                       numpy_data_folder=self.hparams.dataset_dir,
+                                       filter=test_ind,
+                                       transforms=None,  # No augmentation during testing!
+                                       use_confidence_scores=False)
 
         self.train_sampler = RandomSampler(self.train_dataset)
         # TODO: it's best practice to not shuffle the dataset for validation and testing
@@ -191,7 +207,7 @@ class L_STGCN(LightningModule):
 
     def training_epoch_end(self, outputs):
         avg_loss = torch.stack([x['loss'] for x in outputs]).mean()
-        avg_accuracy = sum([x['acc'] for x in outputs])/len(outputs)
+        avg_accuracy = sum([x['acc'] for x in outputs]) / len(outputs)
         tensorboard_logs = {'train_loss': avg_loss, 'train_acc': avg_accuracy}
         return {'avg_train_loss': avg_loss, 'log': tensorboard_logs}
 
@@ -202,26 +218,23 @@ class L_STGCN(LightningModule):
         val_acc = self.compute_accuracy(output, y)
         return {'loss': loss, 'acc': val_acc}
 
-
     def validation_epoch_end(self, outputs):
         avg_loss = torch.stack([x['loss'] for x in outputs]).mean()
-        avg_accuracy = sum([x['acc'] for x in outputs])/len(outputs)
+        avg_accuracy = sum([x['acc'] for x in outputs]) / len(outputs)
         tensorboard_logs = {'val_loss': avg_loss, 'val_acc': avg_accuracy}
         return {'val_loss': avg_loss, 'log': tensorboard_logs}
 
-
     def on_train_end(self):
-        print('Saving Confusion matrix for validation data')
+        print('Saving Confusion matrix')
         confusion_matrix = self.compute_conf_mat(self.val_dataloader())
         fig = plot_conf_matrix(confusion_matrix)
         self.logger.experiment.add_figure("Confusion matrix", fig, global_step=None, close=True, walltime=None)
 
     def on_test_end(self):
-        print('Saving Confusion matrix for test data')
+        print('Saving Confusion matrix')
         confusion_matrix = self.compute_conf_mat(self.test_dataloader())
         fig = plot_conf_matrix(confusion_matrix)
         self.logger.experiment.add_figure("Confusion matrix", fig, global_step=None, close=True, walltime=None)
-
 
     def test_step(self, batch, batch_idx):
         x, y = batch
@@ -233,59 +246,72 @@ class L_STGCN(LightningModule):
     def test_epoch_end(self, outputs):
         # averages across epoch
         avg_loss = torch.stack([x['loss'] for x in outputs]).mean()
-        avg_accuracy = sum([x['acc'] for x in outputs])/len(outputs)
+        avg_accuracy = sum([x['acc'] for x in outputs]) / len(outputs)
         tensorboard_logs = {'avg_test_loss': avg_loss, 'avg_test_acc': avg_accuracy}
         return {'avg_test_loss': avg_loss, 'log': tensorboard_logs}
-
 
     @staticmethod
     def add_model_specific_args(parent_parser):
         parser = ArgumentParser(parents=[parent_parser], add_help=False)
         parser.add_argument('--batch_size', type=int, default=2)
         parser.add_argument('--lr', type=float, default=0.01, help='learning rate')
-        parser.add_argument('--augment_data', type=bool, default=False, help='if passed in, performs random augmentation during training (as in the paper)')
+        parser.add_argument('--augment_data', type=bool, default=False,
+                            help='if passed in, performs random augmentation during training (as in the paper)')
         parser.add_argument('--d', type=int, default=1, help='max distance in spatial neighbourhood')
         parser.add_argument('--gamma', type=int, default=9, help='temporal kernel size')
         parser.add_argument('--dropout_rate', type=float, default=0, help='dropout thresh')
         # edge importance args
-        parser.add_argument('--use_edge_importance', type=bool, default=False, help='if passed in, uses learnable edge importance masks')
-        parser.add_argument('--max_mask_jitter', type=float, default=0.001, help="maximal amount of random perturbation added to the initial edge importance masks")
+        parser.add_argument('--use_edge_importance', type=bool, default=False,
+                            help='if passed in, uses learnable edge importance masks')
+        parser.add_argument('--max_mask_jitter', type=float, default=0.001,
+                            help="maximal amount of random perturbation added to the initial edge importance masks")
 
-        parser.add_argument('--partitioning', type=int, default=0, help='partitioning strategy (0 - unilabeling, 1 - distance labeling, 2 - spatial partitioning, 3 - symmetrical-distance labeling)')
+        parser.add_argument('--partitioning', type=int, default=0,
+                            help='partitioning strategy (0 - unilabeling, 1 - distance labeling, 2 - spatial partitioning, 3 - symmetrical-distance labeling)')
         parser.add_argument('--residual', type=bool, default=False, help='if passed in, uses residual block')
         # data split args
-        parser.add_argument('--data_split', type=int, default=0, help='way to split the data into train/val/test sets (0 - cross-subject, 1 - cross-scenario, 2 - ordinary stratified')
-        parser.add_argument('--train_scenarios', type=list, default=["d1", "d2"], help='scenarios to put into the training set (list of any from d1,d2,d3,d4)')
-        parser.add_argument('--val_scenarios', type=list, default=["d3"], help='scenarios to put into the training set (list of any from d1,d2,d3,d4)')
+        parser.add_argument('--data_split', type=int, default=0,
+                            help='way to split the data into train/val/test sets (0 - cross-subject, 1 - cross-scenario, 2 - ordinary stratified')
+        parser.add_argument('--train_scenarios', type=list, default=["d1", "d2"],
+                            help='scenarios to put into the training set (list of any from d1,d2,d3,d4)')
+        parser.add_argument('--val_scenarios', type=list, default=["d3"],
+                            help='scenarios to put into the training set (list of any from d1,d2,d3,d4)')
+        # early stopping takes min epochs into consideration
+        # parser.add_argument('--min_epochs', type=int, default=500, help='min epochs to run the network for.')
+        # parser.add_argument('--early_stop', type=bool, default=False, help='use early stopping during training')
         # parser.add_argument('--optimizer', type=str, default='adam', help='optimizer to use (adam, sgd)')
-        #TODO rajmund: confidence score, optimizer type missing
+        # TODO rajmund: confidence score, optimizer type missing
         return parser
+
 
 def build_argument_parser():
     parser = ArgumentParser()
     # Add program level args
     parser.add_argument('--device', type=str, default=0)
     parser.add_argument('--nr_classes', type=int, default=6)
-    parser.add_argument('--metadata_file', type=str, default='../datasets/KTH_Action_Dataset/metadata.csv', help='path to the .csv file that contains the metadata')
+    parser.add_argument('--metadata_file', type=str, default='../datasets/KTH_Action_Dataset/metadata.csv',
+                        help='path to the .csv file that contains the metadata')
     parser.add_argument('--dataset_dir', type=str, default='../datasets/KTH_Action_Dataset', help='path to the dataset')
     parser.add_argument('--C_in', type=int, default=2, help='number of channels in the data')
     parser.add_argument('--num_workers', type=int, default=1)
-    parser = L_STGCN.add_model_specific_args(parser) # Add model-specific args
-    parser = Trainer.add_argparse_args(parser) # Add ALL training-specific args
+    # parser.add_argument('--min_epochs', type=int, default=500, help='min epochs to run the network for.')
+    parser = L_STGCN.add_model_specific_args(parser)  # Add model-specific args
+    parser = Trainer.add_argparse_args(parser)  # Add ALL training-specific args
     parser.add_argument('--distance_file', type=str, default='')
-
     # In case of spatial conf. partitioning, pre-calculate distances and store them in a file
 
     return parser
+
 
 if __name__ == "__main__":
     # Parse command-line args
     hparams = build_argument_parser().parse_args()
     model = L_STGCN(hparams)
-    #TODO: check Trainer args: gradient clipping, amp_level for 16-bit precision etc
-    trainer = Trainer.from_argparse_args(hparams, early_stop_callback=early_stop_callback, min_epochs=100)
+    # TODO: check Trainer args: gradient clipping, amp_level for 16-bit precision etc
+    trainer = Trainer.from_argparse_args(hparams, early_stop_callback=early_stop_callback, min_epochs=1, max_epochs=1)
     trainer.fit(model)
     trainer.test()
+
 
 
 
